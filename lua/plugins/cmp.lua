b107849f5f22ebad21c592738987f9849b6d3e60
brad.myrick <brad.myrick@protonmail.com>
2025-07-17 18:05:02 -0500

    added missing files

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,27 @@
+local cmp = require('cmp')
+
+cmp.setup({
+  preselect = cmp.PreselectMode.None,
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+    { name = 'path' },
+    { name = 'buffer' },
+  })
+})
---
+++ lua/plugins/devicons.lua
@@ -0,0 +1,1 @@
+use 'nvim-tree/nvim-web-devicons'
